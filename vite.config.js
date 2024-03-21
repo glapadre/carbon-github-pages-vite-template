26870f95d0f444bea05ff57ea00dd87debe714f7
Drew Glapa <glapadre@msu.edu>
2024-03-21 12:05:51 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/{github-username}/{project-repo}/',
   plugins: [
     react({
       jsxRuntime: 'classic',
